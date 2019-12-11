b48300d40cb82e9fbeede6e3c0d0dbe2c86b7d1d
Sam Noedel <sam.noedel@gmail.com>
2019-12-11 07:20:53 -0800

    Re-organize d6p1 code

--- src/day6.rs
+++ src/day6.rs
@@ -2,10 +2,10 @@ use indextree::{Arena,NodeId};
 use std::collections::HashMap;
 
 #[aoc_generator(day6)]
-pub fn input_generator(input: &str) -> (NodeId, Arena<String>) {
+pub fn input_generator(input: &str) -> Galaxy {
     let mut node_map = HashMap::new();
-    let mut arena = Arena::new();
-    let mut root = None;
+    let mut planets  = Arena::new();
+    let mut root     = None;
 
     input
         .lines()
@@ -15,42 +15,45 @@ pub fn input_generator(input: &str) -> (NodeId, Arena<String>) {
             let to_node: NodeId;
 
             if !node_map.contains_key(from) {
-                from_node = arena.new_node(from.to_string());
+                let planet = Planet::from_str(from);
+                let is_root = planet == Planet::COM;
+                from_node = planets.new_node(planet);
                 node_map.insert(from, from_node);
+                if is_root {
+                    root = Some(from_node);
+                }
             } else {
                 from_node = *node_map.get(from).unwrap();
             }
 
-            if from == "COM" {
-                root = Some(from_node);
-            }
-
             if !node_map.contains_key(to) {
-                to_node = arena.new_node(to.to_string());
+                to_node = planets.new_node(Planet::from_str(to));
                 node_map.insert(to, to_node);
             } else {
                 to_node = *node_map.get(to).unwrap();
             }
 
-            from_node.append(to_node, &mut arena);
+            from_node.append(to_node, &mut planets);
         });
 
     let root = root.expect("No root node detected");
-    (root, arena)
+
+    Galaxy { planets, root }
 }
 
 #[aoc(day6, part1)]
-pub fn solve_part1(input: &(NodeId, Arena<String>)) -> usize {
-    let (root, arena) = input;
+pub fn solve_part1(input: &Galaxy) -> usize {
+    let root = input.root;
+    let arena = &input.planets;
 
-    let mut stack = vec![ *root ];
+    let mut stack = vec![ root ];
     let mut total = 0;
     loop {
         match stack.pop() {
             Some(node) => {
-                let depth = node.ancestors(arena).skip(1).count();
+                let depth = node.ancestors(&arena).skip(1).count();
                 total += depth;
-                node.children(arena).for_each(|c| stack.push(c));
+                node.children(&arena).for_each(|c| stack.push(c));
             },
             None => break,
         }
@@ -58,3 +61,62 @@ pub fn solve_part1(input: &(NodeId, Arena<String>)) -> usize {
 
     total
 }
+
+#[derive(PartialEq)]
+pub enum Planet {
+    COM,
+    YOU,
+    SAN,
+    UNK,
+}
+
+impl Planet {
+    fn from_str(s: &str) -> Planet {
+        match s {
+            "COM" => Planet::COM,
+            "YOU" => Planet::YOU,
+            "SAN" => Planet::SAN,
+            _     => Planet::UNK,
+        }
+    }
+}
+
+pub struct Galaxy {
+    pub planets: Arena<Planet>,
+    pub root:    NodeId,
+}
+
+//#[aoc(day6, part2)]
+//pub fn solve_part2(input: &(NodeId, Arena<String>)) -> usize {
+//    let (root, arena) = input;
+//
+//    // first find either YOU or SAN
+//    let you = find("YOU", *root, arena).expect("Unable to find YOU node");
+//
+//    // next traverse from there to the other
+//    let distance = distance_from(you, "SAN", arena).expect("Unable to find a path");
+//    distance
+//}
+
+// fn find(data: &str, root: NodeId, arena: &Arena<String>) -> Option<NodeId> {
+//     let mut stack = vec![ root ];
+//
+//     loop {
+//         match stack.pop() {
+//             Some(node) => {
+//                 let d = arena.get(node).unwrap().get();
+//                 match d {
+//                     s if s == data => return Some(node),
+//                     _ => node.children(arena).for_each(|c| stack.push(c)),
+//                 }
+//             },
+//             None => break,
+//         }
+//     }
+//
+//     None
+// }
+//
+// fn distance_from(from: NodeId, to: &str, arena: &Arena<String>) -> Option<usize> {
+//     None
+// }
