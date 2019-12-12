484c27485f0d7857b64c3fca804b3887cbe75483
Sam Noedel <sam.noedel@gmail.com>
2019-12-11 21:35:11 -0800

    Clean up d7p1 code

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -12,39 +12,20 @@ pub fn input_generator(input: &str) -> Vec<i32> {
 
 #[aoc(day7, part1)]
 pub fn solve_part1(code: &Vec<i32>) -> i32 {
-    let mut phases = [ 0, 1, 2, 3, 4 ];
-    let inputs = Heap::new(&mut phases);
+    // let mut phases = [ 0, 1, 2, 3, 4 ];
+    // let inputs = Heap::new(&mut phases);
 
-    inputs
+    Heap::new(&mut [ 0, 1, 2, 3, 4, ])
         .map(|inputs| {
-            let mut amplifiers = vec![
-                Amplifier::new(), // Amp A
-                Amplifier::new(), // Amp B
-                Amplifier::new(), // Amp C
-                Amplifier::new(), // Amp D
-                Amplifier::new(), // Amp E
-            ];
-
-            amplifiers
-                .iter_mut()
-                .zip(&inputs)
-                .fold(0, |acc, (amp, phase)| amp.run(code, *phase, acc))
+            inputs
+                .iter()
+                .fold(0, |acc, phase| {
+                    let mem = code.clone();
+                    let mut prog = Program::new(mem, vec![ *phase, acc ], vec![]);
+                    prog.run();
+                    *prog.stdout().get(0).expect("No output produced in amplifier")
+                })
         })
         .max()
         .expect("No output produced from pipeline")
-}
-
-struct Amplifier { }
-
-impl Amplifier {
-    fn new() -> Amplifier {
-        Amplifier { }
-    }
-
-    fn run(&mut self, mem: &Vec<i32>, phase: i32, input: i32) -> i32 {
-        let mem = mem.clone();
-        let mut prog = Program::new(mem, vec![ phase, input ], vec![]);
-        prog.run();
-        *prog.stdout().get(0).expect("No output produced in amplifier")
-    }
 }
